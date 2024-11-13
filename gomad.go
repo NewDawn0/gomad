f6d545d6a4de4a886e68bcc1d7e11a720c62fe08
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-11-13 19:53:46 +0100

    bugfix, value not stored

--- gomad.go
+++ gomad.go
@@ -17,7 +17,8 @@ func newErrTypedErrMonad[T any](err error) TypedErrMonad[T] {
 	var zero T
 	return TypedErrMonad[T]{Val: zero, Err: err}
 }
-func (self TypedErrMonad[T]) Bind(f interface{}, args ...interface{}) TypedErrMonad[T] {
+func (self *TypedErrMonad[T]) Bind(f interface{}, args ...interface{}) *TypedErrMonad[T] {
+	var zero T
 	// Propagate previous error if any
 	if self.Err != nil {
 		return self
@@ -25,15 +26,21 @@ func (self TypedErrMonad[T]) Bind(f interface{}, args ...interface{}) TypedErrMo
 	// Check if f is a function
 	fnVal := reflect.ValueOf(f)
 	if fnVal.Kind() != reflect.Func {
-		return newErrTypedErrMonad[T](fmt.Errorf("Bind expects a function"))
+		self.Err = fmt.Errorf("Bind expects a function")
+		self.Val = zero
+		return self
 	}
 	// Ensure the function returns either `T` and `error`, or just `T`, or just `error`.
 	if fnVal.Type().NumOut() > 2 || (fnVal.Type().NumOut() == 1 && fnVal.Type().Out(0) != reflect.TypeOf((*error)(nil)).Elem() && fnVal.Type().Out(0) != reflect.TypeOf(self.Val)) {
-		return newErrTypedErrMonad[T](fmt.Errorf("Function must return T, error, or just error"))
+		self.Err = fmt.Errorf("Function must return T, error, or just error")
+		self.Val = zero
+		return self
 	}
 	// Check if len of args match to what function f expects
 	if fnVal.Type().NumIn() != len(args) {
-		return newErrTypedErrMonad[T](fmt.Errorf("Bound function expected %d arguments, got %d", fnVal.Type().NumIn(), len(args)))
+		self.Err = fmt.Errorf("Bound function expected %d arguments, got %d", fnVal.Type().NumIn(), len(args))
+		self.Val = zero
+		return self
 	}
 	// Convert args to reflect values and call f
 	fnArgs := make([]reflect.Value, len(args))
@@ -47,15 +54,28 @@ func (self TypedErrMonad[T]) Bind(f interface{}, args ...interface{}) TypedErrMo
 	case 1:
 		// If f only returned just `error` or just `T`
 		if valRes, ok := res[0].Interface().(T); ok {
-			return NewTypedMonad(valRes)
+			self.Val = valRes
+			self.Err = nil
+			return self
 		} else if errRes, ok := res[0].Interface().(error); ok && errRes != nil {
-			return newErrTypedErrMonad[T](errRes)
+			self.Err = errRes
+			self.Val = zero
+			return self
 		}
-		return newErrTypedErrMonad[T](fmt.Errorf("Failed to convert function output to T"))
+		fmt.Println("Failed to convert func to output T")
+		self.Err = fmt.Errorf("Failed to convert function output to T")
+		self.Val = zero
+		return self
 	case 2:
 		// If f returns `T` and `error`
-		if errRes, ok := res[1].Interface().(error); ok && errRes != nil {
-			return newErrTypedErrMonad[T](errRes)
+		if valRes, ok := res[0].Interface().(T); ok {
+			self.Val = valRes
+			self.Err = nil
+			return self
+		} else if errRes, ok := res[1].Interface().(error); ok && errRes != nil {
+			self.Err = errRes
+			self.Val = zero
+			return self
 		}
 	}
 	return self
